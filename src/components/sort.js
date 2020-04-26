4d9c6c05359cc50f7417072294a84640333f4c61
ivlev <ivlev.me@gmail.com>
2020-04-26 22:27:11 +1000

    added changing status for sort buttons

--- src/components/sort.js
+++ src/components/sort.js
@@ -46,6 +46,9 @@ export default class Sort extends AbstractComponent {
 
       this._currenSortType = sortType;
 
+      this.getElement().querySelector(`.sort__button--active`).classList.remove(`sort__button--active`);
+      evt.target.classList.add(`sort__button--active`);
+
       handler(this._currenSortType);
     });
   }
